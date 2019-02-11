0a8924c1a35d7c769fa57755fffa2d1471b67ae6
Ram_ASK <36811723+ASKRamsankar@users.noreply.github.com>
2019-02-11 15:11:35 +0530

    Update knapsack_problem_greedy_method.py

--- knapsack_problem_greedy_method.py
+++ knapsack_problem_greedy_method.py
@@ -5,10 +5,11 @@ ratio=[]
 for i in range(n):
   ratio.append(val[i]/wt[i])
 cost=0
-while W>=0 and len(val)>0:
+while W>=0 and len(ratio)>0:
   if W>=wt[ratio.index(max(ratio))]:
     cost+=val[ratio.index(max(ratio))]
     W-=wt[ratio.index(max(ratio))]
   val.pop(ratio.index(max(ratio)))
   wt.pop(ratio.index(max(ratio)))
+  ratio.pop(ratio.index(max(ratio)))
 print(cost)
